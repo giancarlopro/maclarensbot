69631eeea73704859391be41da4dd32a525cab27
Giancarlo Rocha <giancarloiff@gmail.com>
2018-03-14 21:31:49 -0300

    Changed to flask

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+from telegram.ext import CommandHandler
+from telegram import Bot, Update
+
+app = Flask(__name__)
+
+token = os.environ.get("TOKEN")
+port = int(os.environ.get('PORT', '443'))
+
+def saymyname (bot, update):
+    update.message.reply_text(update.message.from_user.first_name)
+
+def setup():
+    global token
+    bot = Bot(token)
+    
+    dispatcher = Dispatcher(bot, None, workers=0)
+    dispatcher.add_handler(CommandHandler('saymyname', saymyname))
+
+    updater.bot.set_webhook("https://maclarensbot.herokuapp.com/" + token)
+    return dispatcher
+
+@app.route('/' + token, methods=['POST'])
+def webhook ():
+    dispatcher = setup()
+
+    text = request.data
+
+    update = Update.de_json(json.loads(text), bot)
+
+    dispatcher.process_update(update)
