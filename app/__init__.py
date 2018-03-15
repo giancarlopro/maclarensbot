1c193327837825b1361fc0fccc50d234b9d3ea9e
Giancarlo Rocha <giancarloiff@gmail.com>
2018-03-15 11:21:29 -0300

    Test

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ def setup():
     updater.bot.set_webhook("https://maclarensbot.herokuapp.com/" + token)
     return dispatcher
 
-@app.route('/' + str(token), methods=['POST'])
+@app.route('/' + str(token), methods=['GET', 'POST'])
 def webhook ():
     dispatcher = setup()
 
@@ -30,7 +30,7 @@ def webhook ():
     update = Update.de_json(json.loads(text), bot)
 
     dispatcher.process_update(update)
-    return ""
+    return "Ok"
 
 @app.route('/')
 def wellcome ():
