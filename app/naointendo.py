daaf0b5bcf77ff5ab3da2e896a276444a1027a3f
Giancarlo Rocha <giancarloiff@gmail.com>
2018-05-25 10:30:28 -0300

    Refactored random post, and added last_post

--- app/naointendo.py
+++ app/naointendo.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+from random import randint
 
 class NaoIntendo:
     def __init__(self):
         self.base_url = 'http://feeds.feedburner.com/naointendo'
 
-    def random_post(self):
+    def last_post(self):
         """Returns a url for one naointendo post"""
         res = requests.get(self.base_url)
         soup = BeautifulSoup(res.text, 'html.parser')
@@ -21,3 +22,22 @@ class NaoIntendo:
                     pass
 
         return False
+    
+    def random_post(self):
+        """Returns a url for one naointendo post"""
+        res = requests.get(self.base_url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+        for i in range(20):
+            itens = soup.findAll('item')
+            urls = list()
+            desc = itens[i].find('description')
+            if len(desc.text) > 0:
+                img_soup = BeautifulSoup(desc.text, 'html.parser')
+                try:
+                    img = img_soup.find('img')['src']
+                    urls.append(str(img).encode('utf8'))
+                except:
+                    pass
+
+        return urls[randint(0, 19)] if urls else False
