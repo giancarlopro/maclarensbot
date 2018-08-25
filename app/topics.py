e506b570857048931310a26831db951d00c64df5
Giancarlo Rocha <giancarloiff@gmail.com>
2018-08-25 11:12:31 -0300

    Added random topics generator

---
+++ app/topics.py
@@ -0,0 +1,26 @@
+import random
+
+class Topic:
+    @staticmethod
+    def random():
+        topics = [
+            'memes',
+            'john cena',
+            '9gag',
+            'cute cats',
+            'video games',
+            'instagram pictures',
+            'lifestyle',
+            'motivacional',
+            'shia labeouf motivacional',
+            'é hora do show porra',
+            'bambam',
+            'how i met your mother',
+            'breaking bad',
+            'badass',
+            'game of thrones',
+            'arya stark',
+            'starks'
+        ]
+
+        return random.choice(topics)
